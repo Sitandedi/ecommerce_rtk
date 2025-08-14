a15e3f281a5efe4aa9d7b166c5dcdf8809ea963b
Sitandedi <f.nyamsi@gmail.com>
2025-08-14 18:24:31 -0400

    message

--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -1,20 +1,52 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeItemFromCart, clearCart, increaseItemQuantity, decreaseItemQuantity } from './CartSlice';
 import './ShoppingCart.css'; 
 
 const ShoppingCart = () => {
 
-  return (
-    <>
-    <div className="shopping-cart">
-      <h2 className="shopping-cart-title">Shopping Cart</h2>
-      <ul className="cart-items">
-       
-      </ul>
-      <button className="clear-cart-btn">Clear Cart</button>
-    </div>
-  
-    </>
-  );
+    const dispatch = useDispatch();
+    const cartItems = useSelector(state => state.cart.cartItems);
+    const totalAmount = cartItems.reduce((total,item) => total + item.price * item.quantity, 0);
+
+    const handleRemoveItem = iemId => {
+        dispatch(removeItemFromCart(iemId));
+    };
+    
+    const handleClearCart = () => {
+        dispatch(clearCart());
+    };
+
+    const handleIncreaseQuantity = itemId => {
+        dispatch(increaseItemQuantity(itemId));
+    };
+
+    const handleDecreaseQuantity = itemId => {
+        dispatch(decreaseItemQuantity(itemId));
+    };
+
+    return (
+        <>
+            <div className="shopping-cart">
+                <h2 className="shopping-cart-title">Shopping Cart</h2>
+                <ul className="cart-items">
+                    {
+                        cartItems.map(item) => (
+                            <li key={item.id} className='cart-item'>
+                                <span>{item.name} - ${item.price}</span>
+                                <div className='quantity-controls'>
+                                    <button className='quantity-control-btn' onClick={() => handleDecreaseQuantity(item.id)}>-</button>
+                                    <span>{item.quantity}</span>
+                                </div>
+                            </li>
+                        )
+                    }
+                </ul>
+                <button className="clear-cart-btn">Clear Cart</button>
+            </div>
+
+        </>
+    );
 };
 
 export default ShoppingCart;
